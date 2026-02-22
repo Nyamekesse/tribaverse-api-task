179be4e15e1209cb075b361ebb59e153fac24168
Nyamekesse Samuel <nyamekessesamuel@outlook.com>
2026-02-22 18:54:51 +0000

    add TypeOrmModule configuration and import necessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { Post } from './post/entities/post.entity';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqljs',
+      autoSave: true,
+      location: 'database.sqlite',
+      entities: [User, Post],
+      synchronize: true,
+      logging: false,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
