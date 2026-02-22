6fbdbc31b4bd54a460b1a091567c20ba923c500d
Nyamekesse Samuel <nyamekessesamuel@outlook.com>
2026-02-22 19:20:16 +0000

    fix import path for Post entity in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { Post } from './post/entities/post.entity';
+import { Post } from './posts/entities/post.entity';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
