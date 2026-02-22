d4c9b88af6658db13b9286f0978f384e968140a5
Nyamekesse Samuel <nyamekessesamuel@outlook.com>
2026-02-22 17:43:19 +0000

    add post entity with fields for content, userId, and timestamps

---
+++ src/post/entities/post.entity.ts
@@ -0,0 +1,28 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
